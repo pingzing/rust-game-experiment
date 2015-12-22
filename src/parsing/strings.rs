b45a1204ece5d52e1752119dc701d1ed4af9c53e
pingzing <neil.mcalister@futurice.com>
2015-12-22 16:56:29 +0200

    Add beginning of string parser

---
+++ src/parsing/strings.rs
@@ -0,0 +1,8 @@
+fn ordered_inject(base_string: String, Vec<String> args) -> &str {        
+    let mut counter = 0;
+    for curr_char in base_string.chars() {
+        if(curr_char == "{" && curr_char)
+        
+        counter++;        
+    }                    
+}
